4f3ae8b7422ba3945e8ea109876cbc3fbb45288f
Tony Kemp <tony@agrav.net>
2013-02-09 11:42:35 +1100

    Initial commit of base Rails 3.2.11 app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hoap2::Application.config.secret_token = '71e823dff5cce24252ad0a1ab7720e04daa74f020cae192d153dd99a8e52bc302f7d32ee425711add460d68250d2a6ebe9b33eef9b7ad79863512c3395348628'
