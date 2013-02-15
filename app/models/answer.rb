94c20f35bbd7e787af599d60de903badcb81bfe1
Tony Kemp <tony@agrav.net>
2013-02-16 10:58:48 +1100

    Adds Participant and Answer creation methods.

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,4 +3,12 @@ class Answer < ActiveRecord::Base
   attr_accessible :value
   belongs_to :participant
   belongs_to :question
+
+  # Create and Answer record for each Question for the given Participant
+  def self.make_all(participant)
+    questions = Question.select("id, page").order("id ASC")
+    questions.each do |question|
+      a = Answer.create({participant_id: participant.id, question_id: question.id, page: question.page}, as: :creator)
+    end
+  end
 end
--- app/models/participant.rb
+++ app/models/participant.rb
@@ -20,4 +20,32 @@ class Participant < ActiveRecord::Base
     end
     return participant
   end
+
+  # Creates the Participant and blank Answer records for the given participant
+  # number and completion status from the first survey. Only control group and
+  # intervention group participants are created.
+  # Returns nil when given an invalid/unwanted completion status, or the created
+  # Participant object otherwise
+  def self.make(pno, completion_status)
+    case exit_code
+    when "Control"
+      # Control
+      group = 1
+    when "Finished"
+      # Intervention
+      group = 0
+    else
+      # Incomplete or ineligible
+      return nil
+    end
+    participant = self.new({pno: pno, group: group}, as: :creator)
+    begin
+      participant.key = SecureRandom.hex(6)
+      participant.save
+    rescue ActiveRecord::RecordNotUnique
+      retry
+    end
+    Answer.make_all(participant)
+    return participant
+  end
 end
