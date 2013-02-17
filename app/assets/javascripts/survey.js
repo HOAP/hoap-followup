84bd76c15dfa37faad89abe25a10c28550c228dd
Tony Kemp <tony@agrav.net>
2013-02-17 16:20:38 +1100

    Adds response validation.

--- app/assets/javascripts/survey.js
+++ app/assets/javascripts/survey.js
@@ -30,3 +30,51 @@ function configurePage3() {
     }
   });
 }
+
+function missedAlert(form, validator) {
+  var errors = validator.numberOfInvalids();
+  if (errors) {
+    var message = errors == 1
+      ? 'You missed 1 question. It has been highlighted.'
+      : 'You missed ' + errors + ' questions. They have been highlighted.<br/>';
+    $("#dialog-confirm").html(message);
+    $("#dialog-confirm").dialog("open");
+    // Make sure dialog in centred after scrolling up to focus on first missed question.
+    setTimeout("$('#dialog-confirm').dialog('widget').position({my: 'center', at: 'center', of: window});", 20);
+  }
+}
+
+function configureCheck() {
+  jQuery.validator.messages.required = "";
+
+  $("#dialog-confirm").dialog({
+    autoOpen: false,
+    resizable: false,
+    width: 500,
+    modal: true,
+    buttons: {
+      "Go back to missed questions": function() {
+        $( this ).dialog( "close" );
+        $("input:blank").first().focus();
+      }
+    }
+  });
+
+  $("form").validate({
+    highlight: function(element, errorClass, validClass) {
+      $(element).parents("p,tr,ul").addClass(errorClass);
+    },
+    unhighlight: function(element, errorClass, validClass) {
+      $(element).parents("p,tr,ul").removeClass(errorClass);
+    },
+    focusInvalid: true,
+    invalidHandler: missedAlert
+  });
+}
+
+function configureIndex() {
+  jQuery.validator.messages.required = "Invalid participant code - please double check your letter or email and try again.";
+  $("form").validate({
+    wrapper: "div"
+  });
+}
