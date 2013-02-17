850a62534538270029e157ee97d93c2be2275b28
Tony Kemp <tony@agrav.net>
2013-02-17 12:32:03 +1100

    Adds Page 2.

--- app/assets/javascripts/survey.js
+++ app/assets/javascripts/survey.js
@@ -1,3 +1,19 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+// Configure events for the AUDIT page (page 2)
+function configureAudit() {
+  $("#used input:radio").on("change", function() {
+    if ($("#used input:radio:checked").val() == "No") {
+      $(".audit").hide();
+      $(".audit select").removeClass("required");
+    } else {
+      $(".audit").show();
+      $(".audit select").addClass("required");
+    }
+  });
+  $(".audit:last select").on("change", function() {
+    if ($(".audit:last select").val() == "Yes, during the last 6 months") {
+      $(".help").show();
+    } else {
+      $(".help").hide();
+    }
+  })
+}
