734e26166f87f8ce71117d105928510921b1ff67
Tony Kemp <tony@agrav.net>
2013-08-21 19:05:10 +1000

    Adds questions for new page 6.

--- db/seeds.rb
+++ db/seeds.rb
@@ -46,6 +46,13 @@ questions = [
   {page: 5, text: "Did you receive personalised feedback about your drinking on the iPad while you were waiting for your outpatient appointment?", values: ["No", "Yes"]},
   {page: 5, text: "Would you be interested in receiving some personalised feedback about your drinking (this would involve following a link to a website in an email or a letter we send to you)?", values: ["No thanks", "Yes, please send the link to me"]},
   {page: 5, text: "What date did you complete this questionnaire?"},
+  {page: 6, text: "Telephone support from a counsellor.", values: ["No", "Yes"]},
+  {page: 6, text: "Access to a website that supports people who are trying to change their drinking.", values: ["No", "Yes"]},
+  {page: 6, text: "Having results about my drinking sent to my GP.", values: ["No", "Yes"]},
+  {page: 6, text: "Would you be willing to take part in a brief telephone interview about how we might improve the way care is provided to patients with unhealthy drinking?", values: ["No", "Yes"]},
+  {page: 6, text: "The best number to contact me on is:"},
+  {page: 6, text: "The best days and times to call are:"},
+  {page: 6, text: "What date did you complete this questionnaire?"},
   {page: 7, text: "We would be interested in any comments you would like to make about your experience with alcohol or any aspect of being involved in this research."}
 ]
 
